1e3335a5409e71c4448ea78e5bba7078bbbe6e38
Gabriel Lemos <gabriel.faria@sistemapoliedro.com.br>
2019-09-24 08:34:42 -0300

    how to stateful widget works, create and use it

--- my-first-app/myapp/lib/main.dart
+++ my-first-app/myapp/lib/main.dart
@@ -19,7 +19,48 @@ class HomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: BodyWidget(),
+    );
+  }
+}
+
+class BodyWidget extends StatefulWidget {
+  @override
+  _BodyWidgetState createState() => _BodyWidgetState();
+}
+
+class _BodyWidgetState extends State<BodyWidget> {
+  Color color = Colors.blue;
+
+  @override
+  void initState() {
+    // sobrescrive a criação do widget
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // sobrescrive a destruição do widget
+    super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    // inicia algo quando tem a certeza de estado, quando não vai para o initState
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        setState(() {          
+          color = (color == Colors.redAccent ? Colors.blue : Colors.redAccent);
+        });
+      },
+      child: Container(
+        color: color,
+      ),
     );
   }
 }
