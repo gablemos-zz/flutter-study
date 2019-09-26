c14aa5697b7d95b5a8428fce97714bc749e5de7c
Gabriel Lemos <gabriel.faria@sistemapoliedro.com.br>
2019-09-26 13:46:06 -0300

    25 - padding and sizebox, icon as parameter

--- my-first-app/myapp/lib/main.dart
+++ my-first-app/myapp/lib/main.dart
@@ -18,9 +18,10 @@ class MyApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              ButtonIcon(),              
-              SizedBox(height: 50,), //ou  Container(height: 50),
-              ButtonText()
+              ButtonIcon(icon: Icons.arrow_downward),              
+              ButtonText(), 
+              // ButtonIcon(Icons.arrow_upward)
+              ButtonIcon(icon: Icons.arrow_upward)
             ],
           ),
         ),
@@ -30,17 +31,27 @@ class MyApp extends StatelessWidget {
 }
 
 class ButtonIcon extends StatelessWidget {
+
+  IconData icon ;
+  ButtonIcon({this.icon});
+  // ButtonIcon(IconData icons){
+  //   this.icon = icons;
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //width: double.infinity,//estica pela tela toda
-      width: 300,
-      height: 50,  
-      color: Colors.greenAccent,  
-      child: Icon(
-        Icons.add,
-        color: Colors.black26,
-      ),  
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 30),
+      child: Container(
+        //width: double.infinity,//estica pela tela toda
+        width: double.infinity,
+        height: 50,  
+        color: Colors.greenAccent,  
+        child: Icon(
+          this.icon,
+          color: Colors.black26,
+        ),  
+      ),
     );
   }
 }
@@ -48,16 +59,20 @@ class ButtonIcon extends StatelessWidget {
 class ButtonText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 300,
-      height: 50,  
-      color: Colors.deepPurple,  
-      alignment: Alignment.center,
-      child: Text(
-        "Toque aqui!",
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 18
+    return Padding(
+      // padding: const EdgeInsets.only(top: 8.0, left: 30, bottom: 8, right: 30),
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 30),
+      child: Container(
+        width: double.infinity,
+        height: 50,  
+        color: Colors.deepPurple,  
+        alignment: Alignment.center,
+        child: Text(
+          "Toque aqui!",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18
+          ),
         ),
       ),
     );
