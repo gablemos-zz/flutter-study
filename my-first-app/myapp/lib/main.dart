e9c694a1198dee7fe4464e2a098352b77b2777c9
Gabriel Lemos <gabrielelias.lemos@gmail.com>
2019-10-05 21:16:03 -0300

    31 - uncoupling

--- my-first-app/myapp/lib/main.dart
+++ my-first-app/myapp/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/button-text-icon.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -18,10 +20,16 @@ class MyApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              ButtonIcon(icon: Icons.arrow_downward),
-              ButtonText(),
-              ButtonIcon(icon: Icons.arrow_upward),
-              ButtonTextIcon(),
+              ButtonTextIcon(
+                color: Colors.red,
+                text: "Login Google",
+                iconData: Icons.email,
+              ),
+              ButtonTextIcon(
+                color: Colors.blue,
+                text: "Login Facebook",
+                iconData: Icons.tag_faces,
+              ),
               SizedBox(
                 height: 30,
               ),
@@ -53,100 +61,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class ButtonIcon extends StatelessWidget {
-  IconData icon;
-  ButtonIcon({this.icon});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 30),
-      child: Container(
-        decoration: BoxDecoration(
-            color: Colors.greenAccent,
-            borderRadius: BorderRadius.circular(15.0),
-            boxShadow: <BoxShadow>[
-              BoxShadow(
-                  color: Colors.black.withOpacity(0.5),
-                  offset: Offset(3.0, 5.0),
-                  blurRadius: 7)
-            ]),
-        width: double.infinity,
-        height: 50,
-        child: Icon(
-          this.icon,
-          color: Colors.black26,
-        ),
-      ),
-    );
-  }
-}
-
-class ButtonTextIcon extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 30),
-      child: Container(
-        decoration: BoxDecoration(
-            color: Colors.greenAccent,
-            borderRadius: BorderRadius.circular(15.0),
-            boxShadow: <BoxShadow>[
-              BoxShadow(
-                  color: Colors.black.withOpacity(0.5),
-                  offset: Offset(3.0, 5.0),
-                  blurRadius: 7)
-            ]),
-        width: double.infinity,
-        height: 50,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: Icon(
-                Icons.message,
-                color: Colors.black26,
-              ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Text(
-                "Chat",
-                textAlign: TextAlign.left,
-                style: TextStyle(color: Colors.deepPurple, fontSize: 18),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ButtonText extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 30),
-      child: Container(
-        decoration: BoxDecoration(
-            color: Colors.deepPurple,
-            borderRadius: BorderRadius.circular(25.0),
-            boxShadow: <BoxShadow>[
-              BoxShadow(
-                  color: Colors.black.withOpacity(0.5),
-                  offset: Offset(3.0, 5.0),
-                  blurRadius: 7)
-            ]),
-        width: double.infinity,
-        height: 50,
-        alignment: Alignment.center,
-        child: Text(
-          "Olha Mãe, to fazendo um APP",
-          style: TextStyle(color: Colors.white, fontSize: 18),
-        ),
-      ),
-    );
-  }
-}
